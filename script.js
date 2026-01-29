8b97ac5a25d3eb12281ef365626925cdd8af2ff7
james247-ui <mjeeshan61@gmail.com>
2026-01-29 17:27:26 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+const API_KEY = "PASTE_YOUR_PEXELS_API_KEY_HERE";
+const gallery = document.getElementById("gallery");
+
+// Images
+fetch("https://api.pexels.com/v1/curated?per_page=10", {
+  headers: { Authorization: API_KEY }
+})
+.then(res => res.json())
+.then(data => {
+  data.photos.forEach(photo => {
+    const div = document.createElement("div");
+    div.className = "item";
+    div.innerHTML = `
+      <img src="${photo.src.medium}">
+      <a class="download" href="${photo.src.original}" download>Download</a>
+    `;
+    gallery.appendChild(div);
+  });
+});
+
+// Videos
+fetch("https://api.pexels.com/videos/popular?per_page=4", {
+  headers: { Authorization: API_KEY }
+})
+.then(res => res.json())
+.then(data => {
+  data.videos.forEach(video => {
+    const file = video.video_files[0];
+    const div = document.createElement("div");
+    div.className = "item";
+    div.innerHTML = `
+      <video src="${file.link}" controls muted></video>
+      <a class="download" href="${file.link}" download>Download</a>
+    `;
+    gallery.appendChild(div);
+  });
+});
